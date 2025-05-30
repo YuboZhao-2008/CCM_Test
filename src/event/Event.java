ddf30bbf604b6b8c6f19aa3278fc0889fa4674bd
aicheye <contact@seanyang.me>
2025-05-30 13:55:59 -0400

    Implement TimeBlock class with conflict detection and update Event and Schedule classes

--- src/event/Event.java
+++ src/event/Event.java
@@ -1,1 +1,10 @@
 package event;
+
+import time.TimeBlock;
+import time.TimeBlock.Month;
+
+public abstract class Event {
+    public TimeBlock getTimeBlock() {
+        return new TimeBlock(1, Month.JAN, 1);
+    }
+}
--- src/time/Schedule.java
+++ src/time/Schedule.java
@@ -1,5 +1,46 @@
+/**
+ * Schedule implements a chronologically ordered schedule preventing conflicts
+ * 
+ * @author Sean Yang
+ * @since May 30, 2025
+ */
+
 package time;
 
-public class Schedule {
+import java.util.ArrayList;
 
+import event.Event;
+
+public class Schedule {
+    private ArrayList<Event> eventSchedule;
+
+    public boolean isBlockFree(TimeBlock timeBlock) {
+        for (Event event : eventSchedule) {
+            if (event.getTimeBlock().isConflicting(timeBlock)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private int binarySearch(TimeBlock target) {
+        return binarySearch(0, eventSchedule.size() - 1, target);
+    }
+
+    private int binarySearch(int hi, int lo, TimeBlock target) {
+        int mid = (hi + lo) / 2;
+
+        if (eventSchedule.get(mid).getTimeBlock().compareToStart(target) > 0) {
+            hi = lo - 1;
+        } else {
+            lo = hi - 1;
+        }
+
+        return 0;
+    }
+
+    public boolean add(Event event) {
+        return false;
+    }
 }
