c00ae48ac2e18955a4a31f64d989714154463faf
YuboZhao-2008 <bowenzhao2020@gmail.com>
2025-06-01 09:01:10 -0400

    Member outline

--- src/member/Member.java
+++ src/member/Member.java
@@ -4,44 +4,51 @@ import event.*;
 import facility.*;
 
 public abstract class Member {
-    public double BIWEEKLY_BASE;
-    public double MONTHLY_BASE;
-    public double ANNUAL_BASE;
+    public static final double BIWEEKLY_BASE = 20.00;
+    public static final double MONTHLY_BASE = 35.00;
+    public static final double ANNUAL_BASE = 350.00;
 
     protected int id;
     protected int age;
     protected String name;
     protected Schedule registrations;
+    protected PlanType planType;
 
-    enum PlanType {
+    public enum PlanType {
         BIWEEKLY_BASE,
         MONTHLY_BASE,
         ANNUAL_BASE
     }
 
-    public Member (int id, String name, PlanType) {
+    public Member (int id, String name, PlanType plantype) {
         this.id = id;
         this.name = name;
+        this.planType = plantype;
+        this.registrations = new Schedule();
     }
 
-    public double calculateBill()   {
+    public abstract double calculateBill();
 
-    }
-
-    public String membershipDetails()   {
-
+    public String membershipDetails() {
+        return id
+                + " | "
+                + name
+                + " | "
+                + planType
+                + " | $"
+                + String.format("%.2f", calculateBill());
     }
 
     public void advanceHours(int hours)   {
-
+        registrations.advanceHours(hours);
     }
 
     public void printBill() {
-
+        System.out.println(membershipDetails());
     }
 
     public String toString() {
-
+        return membershipDetails();
     }
 
     public boolean registerFor(Event eventName)   {
