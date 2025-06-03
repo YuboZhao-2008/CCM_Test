129d6a891df6f5eca188eeca6541dc9215b7691a
YuboZhao-2008 <bowenzhao2020@gmail.com>
2025-06-03 15:04:40 -0400

    Update Staff.java

---
+++ src/staff/Staff.java
@@ -0,0 +1,37 @@
+package staff;
+
+public abstract class Staff {
+    private int id;
+    private String name;
+    private Schedule shifts;
+
+    public Staff(String name) {
+        this.name = name;
+    }
+
+    public abstract double calculatePay();
+
+    public boolean scheduleShift(Event event) {
+        return this.shifts.add(event);
+    }
+
+    public abstract void printPayroll();
+
+    public abstract String toString();
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
