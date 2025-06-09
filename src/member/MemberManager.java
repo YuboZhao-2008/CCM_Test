2ef0258e340e3e8c9740f8ddbbd9837bc4d90e79
YuboZhao-2008 <bowenzhao2020@gmail.com>
2025-06-09 14:30:38 -0400

    Update MemberManager.java

--- src/member/MemberManager.java
+++ src/member/MemberManager.java
@@ -1,11 +1,55 @@
 package member;
 
+import java.util.*;
+
 public class MemberManager {
-    public Member searchById(int id) {
-        return null;
+    public ArrayList<Member> members = new ArrayList<>();
+    public void addMember(Member member) {
+        member.setId(generateId());
+        members.add(member);
     }
 
-    public Member searchByName(String name) {
-        return null;
+    public int generateId() {
+        if (members.isEmpty()) {
+            return 1;
+        }
+        return members.getLast().getId() + 1;
+    }
+
+    public Member searchById(int id) {
+        return searchByIdRecursive(id, 0, members.size() - 1);
+    }
+
+    private Member searchByIdRecursive(int id, int low, int high) {
+        if (low > high) {
+            return null;
+        }
+        int mid   = (low + high) / 2;
+        int midId = members.get(mid).getId();
+
+        if (midId == id) {
+            return members.get(mid);
+        } else if (midId > id) {
+            return searchByIdRecursive(id, low, mid - 1);
+        } else {
+            return searchByIdRecursive(id, mid + 1, high);
+        }
+    }
+
+    public void printAllBills() {
+        for (Member m : members) {
+            m.printBill();
+        }
+    }
+
+    public void printAlphabetical() {
+        ArrayList<String> sorted = new ArrayList<>();
+        for (Member m : members) {
+            sorted.add(m.getName());
+        }
+        sort(sorted);
+        for (String name : sorted) {
+            System.out.println(name);
+        }
     }
 }
