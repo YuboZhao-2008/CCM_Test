a2d1e000ff4e10522af06ed577789c2c05d80fc8
YuboZhao-2008 <bowenzhao2020@gmail.com>
2025-06-10 13:08:11 -0400

    Update FullTimeStaff.java

--- src/staff/FullTimeStaff.java
+++ src/staff/FullTimeStaff.java
@@ -1,33 +1,88 @@
 package staff;
 
-public class FullTimeStaff extends Staff{
-    public static final double BASE_SALARY = 60000;
-    public static final double YEARLY_RAISE = 0.05;
+/**
+ * Represents a full-time staff member with a base salary and annual raises.
+ * Calculates monthly pay based on years worked.
+ *
+ * @author Yubo-Zhao
+ * @version 1.0
+ * @since 2025-06-04
+ */
+public class FullTimeStaff extends Staff {
+    /**
+     * The base annual salary for all full-time staff.
+     */
+    public static final double BASE_SALARY   = 60000;
 
+    /**
+     * The annual raise rate applied per year worked.
+     */
+    public static final double YEARLY_RAISE  = 0.05;
+
+    /**
+     * Number of years this staff member has worked.
+     */
     private int yearsWorked;
 
-    public FullTimeStaff (String name, int yearsWorked) {
+    /**
+     * Constructs a FullTimeStaff with the given name and years worked.
+     *
+     * @param name         the full name of the staff member
+     * @param yearsWorked  the number of years the staff has been employed
+     */
+    public FullTimeStaff(String name, int yearsWorked) {
         super(name);
         this.yearsWorked = yearsWorked;
     }
 
+    /**
+     * Calculates the monthly pay by adding raises to the base salary
+     * and dividing by 12 months.
+     *
+     * @return the monthly salary amount
+     */
+    @Override
     public double calculatePay() {
-        return (BASE_SALARY + YEARLY_RAISE * yearsWorked)/12;
+        double adjustedSalary = BASE_SALARY + (YEARLY_RAISE * yearsWorked * BASE_SALARY);
+        return adjustedSalary / 12;
     }
 
-    public void printPayroll()  {
-        System.out.println("Your yearly pay is: " + calculatePay() + " years worked: " + yearsWorked);
+    /**
+     * Prints the payroll details to standard output.
+     */
+    @Override
+    public void printPayroll() {
+        System.out.println("Your monthly pay is: " + calculatePay() +
+                " | Years worked: " + yearsWorked);
     }
 
-    public String toString()    {
-        return ("Employee id: " + this.getId() + " name is: " + this.getName() + " years worked: " + yearsWorked);
+    /**
+     * Returns a string representation of this full-time staff member.
+     *
+     * @return a descriptive string including ID, name, and years worked
+     */
+    @Override
+    public String toString() {
+        return "Employee ID: " + getId() +
+                " | Name: " + getName() +
+                " | Years Worked: " + yearsWorked;
     }
 
+    /**
+     * Gets the number of years worked by this staff member.
+     *
+     * @return the years worked
+     */
     public int getYearsWorked() {
         return yearsWorked;
     }
 
-    public void setYearsWorked(int yearsWorked)    {
+    /**
+     * Sets the number of years worked by this staff member.
+     *
+     * @param yearsWorked the new years worked value
+     */
+    public void setYearsWorked(int yearsWorked) {
         this.yearsWorked = yearsWorked;
     }
 }
