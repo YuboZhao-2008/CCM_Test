c31d189411e1526b11d2becad3c789ee7172ed9e
YuboZhao-2008 <bowenzhao2020@gmail.com>
2025-06-10 13:08:32 -0400

    Update PartTimeStaff.java

--- src/staff/PartTimeStaff.java
+++ src/staff/PartTimeStaff.java
@@ -1,52 +1,131 @@
 package staff;
-public class PartTimeStaff extends Staff{
+
+/**
+ * Represents a part-time staff member who is paid by the hour.
+ * Calculates weekly pay up to a maximum number of hours.
+ *
+ * @author Yubo-Zhao
+ * @version 1.0
+ * @since 2025-06-03
+ */
+public class PartTimeStaff extends Staff {
+    /**
+     * The number of hours this staff has worked in the week.
+     */
     private int hoursWorked;
+
+    /**
+     * The hourly wage for this staff.
+     */
     private double hourlySalary;
+
+    /**
+     * The maximum number of hours this staff is allowed to work per week.
+     */
     private int maxWeeklyHours;
 
-    public PartTimeStaff (String name, int hoursWorked, double hourlySalary, int maxWeeklyHours)    {
+    /**
+     * Constructs a new PartTimeStaff with the given name and pay parameters.
+     *
+     * @param name            the staff member's name
+     * @param hoursWorked     the initial hours worked
+     * @param hourlySalary    the wage per hour
+     * @param maxWeeklyHours  the cap on hours per week
+     */
+    public PartTimeStaff(String name, int hoursWorked, double hourlySalary, int maxWeeklyHours) {
         super(name);
-        this.hoursWorked = hoursWorked;
-        this.hourlySalary = hourlySalary;
+        this.hoursWorked    = hoursWorked;
+        this.hourlySalary   = hourlySalary;
         this.maxWeeklyHours = maxWeeklyHours;
     }
 
-    public double calculatePay()    {
-        if (hoursWorked >= maxWeeklyHours)   {
+    /**
+     * Calculates the weekly pay. If hoursWorked exceeds maxWeeklyHours,
+     * it is capped before calculating pay.
+     *
+     * @return the total pay for this week
+     */
+    @Override
+    public double calculatePay() {
+        if (hoursWorked > maxWeeklyHours) {
             hoursWorked = maxWeeklyHours;
         }
         return hoursWorked * hourlySalary;
     }
 
+    /**
+     * Prints this staff member's payroll information to stdout.
+     */
+    @Override
     public void printPayroll() {
-        System.out.println("Your pay is: " + calculatePay() + " years worked: " + hoursWorked);
+        System.out.println("Your pay is: " + calculatePay() +
+                " | Hours worked: " + hoursWorked);
     }
 
-    public String toString()    {
-        return ("staff id: " + this.getId() + " staff name: " + this.getName() + " employee hourly Salary: " + hourlySalary + " employee maximum hours per week: " + maxWeeklyHours);
+    /**
+     * Returns a string representation of this PartTimeStaff.
+     *
+     * @return a string containing ID, name, salary info, and max hours
+     */
+    @Override
+    public String toString() {
+        return "Staff ID: " + getId() +
+                " | Name: " + getName() +
+                " | Hourly Salary: " + hourlySalary +
+                " | Max Weekly Hours: " + maxWeeklyHours;
     }
 
-    public int getHoursWorked ()    {
+    /**
+     * Gets the number of hours worked.
+     *
+     * @return hours worked this week
+     */
+    public int getHoursWorked() {
         return hoursWorked;
     }
 
-    public void setHoursWorked (int hoursWorked)    {
+    /**
+     * Sets the number of hours worked.
+     *
+     * @param hoursWorked the new hours worked value
+     */
+    public void setHoursWorked(int hoursWorked) {
         this.hoursWorked = hoursWorked;
     }
 
+    /**
+     * Gets the hourly wage.
+     *
+     * @return the hourly salary
+     */
     public double getHourlySalary() {
         return hourlySalary;
     }
 
-    public void setHourlySalary (double hourlySalary)   {
+    /**
+     * Sets the hourly wage.
+     *
+     * @param hourlySalary the new hourly salary
+     */
+    public void setHourlySalary(double hourlySalary) {
         this.hourlySalary = hourlySalary;
     }
 
-    public int getMaxWeeklyHours()  {
+    /**
+     * Gets the maximum weekly hours allowed.
+     *
+     * @return the max weekly hours
+     */
+    public int getMaxWeeklyHours() {
         return maxWeeklyHours;
     }
 
-    public void setMaxWeeklyHours (int maxWeeklyHours)  {
+    /**
+     * Sets the maximum weekly hours allowed.
+     *
+     * @param maxWeeklyHours the new max weekly hours
+     */
+    public void setMaxWeeklyHours(int maxWeeklyHours) {
         this.maxWeeklyHours = maxWeeklyHours;
     }
 }
