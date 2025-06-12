e4e5d749b574933bab994562ba42dd6621d4d812
YuboZhao-2008 <bowenzhao2020@gmail.com>
2025-06-12 13:05:12 -0400

    Update BookMenu.java

--- src/main/menu/BookMenu.java
+++ src/main/menu/BookMenu.java
@@ -8,39 +8,72 @@
 
 package main.menu;
 
+import main.CommunityCentreRunner;
 import main.CommunityCentreRunner.MenuStatus;
 import main.ValidateInput;
+import event.Event;
+import member.Member;
+import staff.Staff;
 
 public class BookMenu {
-    // show the menu
     public static MenuStatus show() {
-        System.out.println("What would you like to book?");
-        System.out.println("(1) Book a Member");
-        System.out.println("(2) ");
-        System.out.println("(3) Modify Facility");
-        System.out.println("(4) Modify Event");
-        System.out.println("-");
+        System.out.println("What would you like to do?");
+        System.out.println("(1) Sign up member for an event");
+        System.out.println("(2) Assign staff to an event");
         System.out.println("(0) Back");
 
-        int modifyChoice = ValidateInput.menu(4);
-        main.CommunityCentreRunner.separate();
+        int choice = ValidateInput.menu(2);
+        CommunityCentreRunner.separate();
 
-        switch (modifyChoice) {
-            case 1 -> {
+        switch (choice) {
+            case 1: {
+                System.out.println("Enter event ID");
+                System.out.print(" > ");
+                int eventId = ValidateInput.posInt();
+                Event event = CommunityCentreRunner.getEventManager().searchById(eventId);
+                if (event == null) {
+                    System.out.println("Event with ID " + eventId + " not found.");
+                    break;
+                }
 
+                System.out.println("Enter member ID");
+                System.out.print(" > ");
+                int memberId = ValidateInput.posInt();
+                Member member = CommunityCentreRunner.getMemberManager().searchById(memberId);
+                if (member == null) {
+                    System.out.println("Member with ID " + memberId + " not found.");
+                    break;
+                }
+
+                event.registerParticipant(member);
+                System.out.println("Member " + memberId + " signed up for event " + eventId + ".");
+                break;
             }
-            case 2 -> {
+            case 2: {
+                System.out.println("Enter event ID");
+                System.out.print(" > ");
+                int eventId2 = ValidateInput.posInt();
+                Event event2 = CommunityCentreRunner.getEventManager().searchById(eventId2);
+                if (event2 == null) {
+                    System.out.println("Event with ID " + eventId2 + " not found.");
+                    break;
+                }
 
-            }
-            case 3 -> {
+                System.out.println("Enter staff ID");
+                System.out.print(" > ");
+                int staffId = ValidateInput.posInt();
+                Staff staff = CommunityCentreRunner.getStaffManager().searchById(staffId);
+                if (staff == null) {
+                    System.out.println("Staff with ID " + staffId + " not found.");
+                    break;
+                }
 
+                event2.assignStaff(staff);
+                System.out.println("Staff " + staffId + " assigned to event " + eventId2 + ".");
+                break;
             }
-            case 4 -> {
-
-            }
-            case 0 -> {
+            case 0:
                 return MenuStatus.BACK;
-            }
         }
 
         return MenuStatus.CONTINUE;
